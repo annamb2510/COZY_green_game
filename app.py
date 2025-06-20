8ee937a57b9282d2562f1a1dadfc23d4ded2e092
annamb2510 <annamb2510@gmail.com>
2025-06-20 21:39:34 +0200

    Fix: aggiunto return alla route /obiettivi

--- app.py
+++ app.py
@@ -156,3 +156,5 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+    return render_template("obiettivi.html", obiettivi=obiettivi_lista, raggiunti=raggiunti)
+
