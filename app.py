0fcb2fec79033868441301cdf127c853cee1d3a4
annamb2510 <annamb2510@gmail.com>
2025-07-16 20:58:54 +0200

    admin

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from flask import (
     Flask, session, render_template,
     request, redirect, url_for, flash
 )
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+# Carica variabili ambiente da .env
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'vacanza-secret-key')
